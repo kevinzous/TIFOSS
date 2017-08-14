7fd784150c89d806ab9b4ca819421fa7672e871f
Brunel <jules.brunel@supelec.fr>
2017-08-14 13:03:46 +0200

    Initial commit

---
+++ informations/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+
+
+urlpatterns=[
+    
+    url(r'^planning',TemplateView.as_view(template_name='informations/planning.html')),
+    url(r'^partenariats',TemplateView.as_view(template_name='informations/partenariats.html')),
+    url(r'^leMotDuDirecteur',TemplateView.as_view(template_name='informations/leMotDuDirecteur.html')),
+    url(r'^teaser',TemplateView.as_view(template_name='informations/teaser.html')),
+    
+    url(r'^commentSYRendre',TemplateView.as_view(template_name='informations/commentSYRendre.html')),
+    url(r'^reglement',TemplateView.as_view(template_name='informations/reglement.html')),
+    url(r'^contacts',TemplateView.as_view(template_name='informations/contacts.html')),
+    url(r'^tarifs',TemplateView.as_view(template_name='informations/tarifs.html')),
+    
+    url(r'^soiree',TemplateView.as_view(template_name='informations/soiree.html')),
+    url(r'^activites',TemplateView.as_view(template_name='informations/activites.html')),
+
+    url(r'^galeriePhotos',TemplateView.as_view(template_name='informations/galeriePhotos.html')),
+    url(r'^palmares',TemplateView.as_view(template_name='informations/palmares.html')),
+    
+    url(r'^participer', TemplateView.as_view(template_name='informations/participer.html')),
+    
+    url(r'',TemplateView.as_view(template_name='informations/pageAccueil.html')),
+    
+    ]
+        
---
+++ participer/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import joueur, equipe, forfait
+
+admin.site.register(joueur)
+admin.site.register(equipe)
+
