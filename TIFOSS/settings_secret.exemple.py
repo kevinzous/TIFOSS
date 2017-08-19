d5ab2a63eea84624bfb88be4764a474ca710e2be
guimoz <me@guimoz.fr>
2017-08-19 18:35:38 +0200

    Restauration de la SECRET_KEY de la DB de dev

--- TIFOSS/settings_secret.exemple.py
+++ TIFOSS/settings_secret.exemple.py
@@ -3,12 +3,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # Pour générer la clé il faut éxécuter le fichier creation_secret_key.py
-SECRET_KEY = 'a générer'
+SECRET_KEY = '!n1cfo%$e!@_3-c5-u^y#-_3mdf$0330j-*xsikxhz$!)zx2k!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
