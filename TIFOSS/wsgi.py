472929cb6ffa1f7d29c265c2cc3a7a8e9da48339
guimoz <me@guimoz.fr>
2017-08-19 17:24:17 +0200

    Ajout du dossier au path

--- TIFOSS/wsgi.py
+++ TIFOSS/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
+from os.path import dirname
+import sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append(dirname(dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TIFOSS.settings")
 
 application = get_wsgi_application()
